3ae4a45545ebe96506a4a9ad3328583eef0368a0
zhu john <zhu john>
2016-07-12 17:37:08 +0800

    first commit

---
+++ ContactManager/Model/States.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContactManager.Model
+{
+    public static class States
+    {
+        private static readonly List<string> _names;
+        static States()
+        {
+            _names = new List<string>(50);
+
+            _names.Add("Alababa");
+            _names.Add("Alaska");
+            _names.Add("Arizona");
+            _names.Add("zzl");
+            _names.Add("juan");
+            _names.Add("ruby");
+        }
+
+        public static IList<string> GetNames()
+        {
+            return _names;
+        }
+    }
+}
---
+++ ContactManager/Notifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace ContactManager
+{
+    [Serializable]
+    public abstract class Notifier : INotifyPropertyChanged
+    {
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
