b48f68a7583a5cfa1aa9dab53897ee8fd0a7a035
formula123 <zuzelian123456@gmail.com>
2016-07-18 21:53:04 +0800

    add mediaviewer example

---
+++ MediaViewer/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MediaViewer.Presenters;
+
+namespace MediaViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new ApplicationController(this);
+        }
+
+        public ApplicationController Controller
+        {
+            get { return (ApplicationController)DataContext; }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Controller.ShowMenu();
+        }
+
+        private void Header_Click(object sender, RoutedEventArgs e)
+        {
+            Controller.ShowMenu();
+        }
+
+        public void TransitionTo(object view)
+        {
+            currentView.Content = view;
+        }
+    }
+}
---
+++ MediaViewer/Presenters/ApplicationController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+
+namespace MediaViewer.Presenters
+{
+    public class ApplicationController
+    {
+        private readonly MainWindow _shell;
+        
+        public ApplicationController(MainWindow shell)
+        {
+            _shell = shell;
+        }
+
+        public void ShowMenu()
+        {
+
+        }
+
+        public void DisplayInShell(object view)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            _shell.TransitionTo(view);
+        }
+
+        public string RequestDirectoryFromUser()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            dialog.Title = "Please choose a folder";
+            dialog.CheckFileExists = false;
+            dialog.FileName = "{Get Folder}";
+            dialog.Filter = "Folder|no.files";
+            
+            if((bool)dialog.ShowDialog())
+            {
+                string path = Path.GetDirectoryName(dialog.FileName);
+                if (!string.IsNullOrEmpty(path))
+                    return path;
+            }
+
+            return string.Empty;
+        }
+    }
+}
