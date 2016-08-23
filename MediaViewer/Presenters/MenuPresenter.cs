8d4e312b0e74aeba253800d18c829b7009d08268
formula123 <zuzelian123456@gmail.com>
2016-08-23 11:30:26 +0800

    MediaViewr: review MediaViewer code

--- MediaViewer/Presenters/MenuPresenter.cs
+++ MediaViewer/Presenters/MenuPresenter.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using MediaViewer.Views;
 using System.Windows.Controls;
 using MediaViewer.Model;
